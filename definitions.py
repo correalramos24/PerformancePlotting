5c357e2390f630d25d5ad54cae065e828e326b2d
Victor Correal <victor.correal@bsc.es>
2023-07-30 04:05:13 +0200

    Lot of refactor, better structure

--- definitions.py
+++ definitions.py
@@ -1,10 +1,11 @@
-from typing import List, Dict
+from typing import List
+import matplotlib.pyplot as plt
+from utils import log
 
 
 class Experiment:
     def __init__(self, throughput=None, throughput_unit: str = "throughput",
-                 time=None, time_unit: str = "s",
-                 name: str = None, **params) -> None:
+                 time=None, time_unit: str = "s", name: str = None, **params) -> None:
 
         self.name = name
         self.params = params
@@ -20,79 +21,148 @@ class Experiment:
             self.throughputUnit = throughput_unit
 
         if name is None and params is not None:
-            print("INFO: name not defined, using first config...", end=" ")
             fst_key = list(params.keys())[0]
             fst_val = params[fst_key]
-            print(f"set name {fst_val}_{fst_key}")
+            log[0](f'Name not defined, using the first configuration as name: {fst_val}_{fst_key}\n')
             self.name = f"{fst_val}_{fst_key}"
 
-    def get_config_by_key(self, key):
-        return int(self.params[key])
+    def get_exp_parameter(self, key):
+        return self.params[key]
 
-    def get_experiment_result(self):
+    def add_exp_parameter(self, key, value):
+        self.params[key] = value
+
+    def has_exp_parameter(self, key):
+        return key in self.params.keys()
+
+    def get_exp_result(self):
         return self.throughput
 
-    def get_experiment_unit(self):
+    def get_exp_result_unit(self):
         return self.throughputUnit
 
+    def get_params_names(self):
+        ret = "Name throughput (throughputUnit) Params:"
+        for k in self.params.keys():
+            ret += f" {k}"
+        return ret
+
     def __repr__(self) -> str:
-        ret = f"{self.name} -> {self.throughput} ({self.throughputUnit})"
+        ret = f"{self.name} {self.throughput} ({self.throughputUnit}) Params: "
+        for k in self.params.values():
+            ret += f"{k} "
         return ret
 
 
 class ExperimentCollection:
 
-    def __init__(self) -> None:
+    def __init__(self, experiments: List[Experiment] = None, file_name: str = None) -> None:
+        if experiments is None:
+            experiments = []
         self.scal_config: str = ""
-        self.scal: Dict = {}
-        self.results: List[Experiment] = []
+        self.results: List[Experiment] = experiments
+        self.file_name = file_name
+
+    @classmethod
+    def from_txt_file(cls, file_name):
+        aux_exp = []
+        with open(file_name) as f:
+            colum_names = []
+            for line_num, line in enumerate(f.readlines()):
+                if "#" in line or len(line.split()) == 0:
+                    continue  # found comment
+
+                if "$" in line:
+                    line = line.replace("$", "")
+                    colum_names = line.split()
+                    log[0](f"Detected {colum_names} fields at file {file_name}\n")
+                    if "time" not in colum_names and "throughput" not in colum_names:
+                        raise Exception("Time nor throughput set in the column names")
+                elif colum_names is not None:
+                    data = line.split()
+                    if len(data) != len(colum_names):
+                        log[1](f"WARNING: Bad column count at line {line_num}, skipping...\n")
+                        print(data)
+                    else:
+                        d = {}
+                        for i, x, in enumerate(colum_names):
+                            d[x] = data[i]
+                        aux_exp.append(Experiment(**d))
+                else:
+                    raise f"Unable to find column names in input {file_name}"
+
+        return cls(aux_exp, file_name)
 
     def add_experiment(self, e: Experiment):
         self.results.append(e)
 
-    def compute_scalability(self, in_base) -> {}:
-        if len(self.results) < 1:
-            print("WARNING: Empty experiments collection, can't compute anything")
-            return
-        self.scal_config = in_base
-        # Get the minimum config of all the experiments:
-        min_config = self.results[0].get_config_by_key(in_base)
-        min_val = self.results[0].get_experiment_result()
-        for e in self.results:
-            if min_config > e.get_config_by_key(in_base):
-                min_config = e.get_config_by_key(in_base)
-                min_val = e.get_experiment_result()
-
-        print(f"INFO: Using {in_base} {min_config} as the reference with result {min_val}.")
-
-        self.scal = {}  # in_base -> (expected, real) scal X
-        for e in self.results:
-            config = e.get_config_by_key(in_base)
-            self.scal[config] = (self._expected_sp(min_config, config),
-                                 self._real_sp(min_val, e.get_experiment_result())
-                                 )
-        return self.scal
-
-    def compute_efficiency(self):
-        pass
-
-    def print_experiment(self):
+    def print_experiments(self):
+        print(self.results[0].get_params_names())
         list(map(lambda e: print(repr(e)), self.results))
 
-    def print_scal(self):
-        if self.scal is not None:
-            print(f"{self.scal_config} Expected Sp(X) Real Sp(X)")
-            for key in self.scal:
-                print(key, self.scal[key][0], self.scal[key][1])
+    def print_scal(self, in_base):
+        self.__compute_scalability(in_base)
+        self.results.sort(key=lambda ex: ex.get_exp_parameter(in_base))
+        print(f"{in_base} Expected Sp(X) Real Sp(X)")
+        for x in self.results:
+            print(x)
+
+    def plot_scalability(self, in_base, as_categorical=False, save_name=None):
+        self.__compute_scalability(in_base)
+
+        self.results.sort(key=lambda ex: ex.get_exp_parameter(in_base))
+
+        cfg, real, expected = [], [], []
+        for exp in self.results:
+            cfg.append(exp.get_exp_parameter(in_base))
+            real.append(exp.get_exp_parameter(f"expected_scal_by{in_base}"))
+            expected.append(exp.get_exp_parameter(f"real_scal_by{in_base}"))
+
+        if not as_categorical:
+            cfg = [int(x) for x in cfg]
+        plt.figure()
+        plt.plot(cfg, real, label="Real Sp(X)")
+        plt.plot(cfg, expected, label="Expected Sp(X)")
+        plt.ylabel("Speedup (X)")
+        plt.xlabel(in_base)
+        plt.title(f"Scaling {in_base}")
+        plt.legend()
+        if save_name is not None:
+            log[0](f"Saving plot to {save_name}\n")
+            plt.savefig(save_name + ".png")
         else:
-            print("Scalability not set")
+            plt.show()
+
+    def __compute_scalability(self, in_base):
+        if len(self.results) < 1:
+            log[1]("Empty experiments collection, can't compute anything\n")
+            return
+
+        if self.results[0].has_exp_parameter(f"expected_scal_by{in_base}"):
+            log[0](f"Caching scalability results for {in_base}\n")
+            return
+
+        # Get the minimum config of all the experiments:
+        min_config_obj = min(self.results, key=lambda exp: exp.get_exp_parameter(in_base))
+        min_config = min_config_obj.get_exp_parameter(in_base)
+        min_val = min_config_obj.get_exp_result()
+
+        log[0](f"Using {in_base} {min_config} as the reference with result {min_val}.\n")
+
+        for e in self.results:
+            e_cfg = e.get_exp_parameter(in_base)
+            e.add_exp_parameter(f"expected_scal_by{in_base}", self._expected_sp(min_config, e_cfg))
+            e.add_exp_parameter(f"real_scal_by{in_base}", self._real_sp(min_val, e.get_exp_result()))
+
+    def __compute_efficiency(self):
+        pass
 
     @staticmethod
     def _expected_sp(min_config, x_config):
         # expected Sp = x_config / min_config
-        return x_config / min_config
+        return float(x_config) / float(min_config)
 
     @staticmethod
     def _real_sp(min_result, x_result):
         # real Sp = throughput_min / throughput_x
-        return x_result / min_result
+        return float(x_result) / float(min_result)
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from readers import *
-from plotting import *
+from definitions import *
+from utils import setLogging
 
 
 def main():
     # Argument parsing
+    setLogging(False)
 
     # Process data
-    d = ReadFromTxt("sampleInput/inputOmp.txt").parse()
+    f_name = "sampleInput/inputOmp.txt"
+    d = ExperimentCollection.from_txt_file(f_name)
+    print(f"Experiment results from file {f_name}")
+    d.print_experiments()
+    d.print_scal("OMP")
+    print("Done")
 
-    d.print_experiment()
-    d.compute_scalability("OMP")
-    d.print_scal()
     # Plot data
-
+    print("Plotting data to a file...")
+    d.plot_scalability(in_base="OMP", as_categorical=False, save_name="myPlot")
+    d.plot_scalability(in_base="OMP", as_categorical=True, save_name="myPlotAsCategorical")
+    print("Done")
     exit(0)
 
 
